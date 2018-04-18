f0094e884280d02f4e672c3829060b1c0945b1fa
William Yates <killowatt2213@gmail.com>
2018-04-18 14:24:06 -0400

    Readded oculus library for reworked integration

--- source/ShaderDude/Main.cpp
+++ source/ShaderDude/Main.cpp
@@ -22,6 +22,9 @@
 #include "UI/TextEditor.h"
 #include "UI/UserInterface.h"
 
+// Oculus
+#include <LibOVR/OVR_CAPI.h>
+
 bool UIManager::g_MouseJustPressed[3] = { false, false, false };
 
 int main()
@@ -31,7 +34,6 @@ int main()
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
@@ -47,7 +49,24 @@ int main()
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	// Deadass Initialize IMGUI
+	// Initialize OculusVR
+	ovrResult result = ovr_Initialize(nullptr);
+	if (OVR_FAILURE(result))
+		return -1;
+
+	ovrSession session;
+	ovrGraphicsLuid luid;
+	result = ovr_Create(&session, &luid);
+	if (OVR_FAILURE(result))
+	{
+		ovr_Shutdown();
+		return -1;
+	}
+
+	ovrHmdDesc desc = ovr_GetHmdDesc(session);
+	ovrSizei resolution = desc.Resolution;
+
+	// Initialize ImGui
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 
@@ -58,9 +77,9 @@ int main()
 	//ImGui::StyleColorsClassic();
 
 	// Load Fonts
-
 	io.Fonts->AddFontFromFileTTF("dependencies/DroidSans.ttf", 16.0f);
 
+	// Set our style
 	ImGui::GetStyle().WindowRounding = 0.0f;
 
 
@@ -85,59 +104,14 @@ int main()
 	TextEditor editor;
 	auto lang = TextEditor::LanguageDefinition::CPlusPlus();
 
-	// set your own known preprocessor symbols...
-	static const char* ppnames[] = { "NULL", "PM_REMOVE",
-		"ZeroMemory", "DXGI_SWAP_EFFECT_DISCARD", "D3D_FEATURE_LEVEL", "D3D_DRIVER_TYPE_HARDWARE", "WINAPI","D3D11_SDK_VERSION", "assert" };
-	// ... and their corresponding values
-	static const char* ppvalues[] = {
-		"#define NULL ((void*)0)",
-		"#define PM_REMOVE (0x0001)",
-		"Microsoft's own memory zapper function\n(which is a macro actually)\nvoid ZeroMemory(\n\t[in] PVOID  Destination,\n\t[in] SIZE_T Length\n); ",
-		"enum DXGI_SWAP_EFFECT::DXGI_SWAP_EFFECT_DISCARD = 0",
-		"enum D3D_FEATURE_LEVEL",
-		"enum D3D_DRIVER_TYPE::D3D_DRIVER_TYPE_HARDWARE  = ( D3D_DRIVER_TYPE_UNKNOWN + 1 )",
-		"#define WINAPI __stdcall",
-		"#define D3D11_SDK_VERSION (7)",
-		" #define assert(expression) (void)(                                                  \n"
-		"    (!!(expression)) ||                                                              \n"
-		"    (_wassert(_CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (unsigned)(__LINE__)), 0) \n"
-		" )"
-	};
-
-	for (int i = 0; i < sizeof(ppnames) / sizeof(ppnames[0]); ++i)
-	{
-		TextEditor::Identifier id;
-		id.mDeclaration = ppvalues[i];
-		lang.mPreprocIdentifiers.insert(std::make_pair(std::string(ppnames[i]), id));
-	}
-
-	// set your own identifiers
-	static const char* identifiers[] = {
-		"HWND", "HRESULT", "LPRESULT","D3D11_RENDER_TARGET_VIEW_DESC", "DXGI_SWAP_CHAIN_DESC","MSG","LRESULT","WPARAM", "LPARAM","UINT","LPVOID",
-		"ID3D11Device", "ID3D11DeviceContext", "ID3D11Buffer", "ID3D11Buffer", "ID3D10Blob", "ID3D11VertexShader", "ID3D11InputLayout", "ID3D11Buffer",
-		"ID3D10Blob", "ID3D11PixelShader", "ID3D11SamplerState", "ID3D11ShaderResourceView", "ID3D11RasterizerState", "ID3D11BlendState", "ID3D11DepthStencilState",
-		"IDXGISwapChain", "ID3D11RenderTargetView", "ID3D11Texture2D", "TextEditor" };
-	static const char* idecls[] =
-	{
-		"typedef HWND_* HWND", "typedef long HRESULT", "typedef long* LPRESULT", "struct D3D11_RENDER_TARGET_VIEW_DESC", "struct DXGI_SWAP_CHAIN_DESC",
-		"typedef tagMSG MSG\n * Message structure","typedef LONG_PTR LRESULT","WPARAM", "LPARAM","UINT","LPVOID",
-		"ID3D11Device", "ID3D11DeviceContext", "ID3D11Buffer", "ID3D11Buffer", "ID3D10Blob", "ID3D11VertexShader", "ID3D11InputLayout", "ID3D11Buffer",
-		"ID3D10Blob", "ID3D11PixelShader", "ID3D11SamplerState", "ID3D11ShaderResourceView", "ID3D11RasterizerState", "ID3D11BlendState", "ID3D11DepthStencilState",
-		"IDXGISwapChain", "ID3D11RenderTargetView", "ID3D11Texture2D", "class TextEditor" };
-	for (int i = 0; i < sizeof(identifiers) / sizeof(identifiers[0]); ++i)
-	{
-		TextEditor::Identifier id;
-		id.mDeclaration = std::string(idecls[i]);
-		lang.mIdentifiers.insert(std::make_pair(std::string(identifiers[i]), id));
-	}
 	editor.SetLanguageDefinition(lang);
 	//editor.SetPalette(TextEditor::GetLightPalette());
 
 	// error markers
 	TextEditor::ErrorMarkers markers;
-	markers.insert(std::make_pair<int, std::string>(6, "Example error here:\nInclude file not found: \"TextEditor.h\""));
-	markers.insert(std::make_pair<int, std::string>(41, "Another example error"));
-	editor.SetErrorMarkers(markers);
+	//markers.insert(std::make_pair<int, std::string>(6, "Example error here:\nInclude file not found: \"TextEditor.h\""));
+	//markers.insert(std::make_pair<int, std::string>(41, "Another example error"));
+	//editor.SetErrorMarkers(markers);
 
 	// "breakpoint" markers
 	//TextEditor::Breakpoints bpts;
@@ -158,9 +132,6 @@ int main()
 	// imgui]
 	bool show_demo_window = false;
 	bool show_another_window = false;
-
-
-	float clear_color[3] = { 0, 0, 0 };
 
 	bool running = true;
 	while (!glfwWindowShouldClose(window))
@@ -276,11 +247,13 @@ int main()
 		{
 
 			ImGui::Begin("Help");
+			ImGui::SetWindowSize(ImVec2(320, 160), ImGuiCond_FirstUseEver);
 			ImGui::Text("Coming Soon");
 			ImGui::End();
 		}
 		{
 			ImGui::Begin("Control Panel");
+			ImGui::SetWindowSize(ImVec2(512, 768), ImGuiCond_FirstUseEver);
 			static float f = 0.0f;
 			static int counter = 0;
 			ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
@@ -371,14 +344,9 @@ int main()
 		glViewport(0, 0, width, height);
 	}
 
-	//glDeleteProgram(shaderProgram);f
-	//glDeleteShader(fragmentShader);
-	//glDeleteShader(vertexShader);
-
-	//glDeleteBuffers(1, &vbo);
-
-	//glDeleteVertexArrays(1, &vao);
-
+	// Deinit Oculus
+	ovr_Destroy(session);
+	ovr_Shutdown();
 
 	return 0;
 }
